829aee6205028eb35307e85d76c78703e9848260
ACER <210220@softwarica.edu.np>
2025-02-21 17:27:21 +0545

    ver

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
-import BlogList from "@/app/BlogList/page";
-import Nav from "@/app/components/Nav";
+import Nav from "./components/Nav";
+import BlogList from "./BlogList/page";
 
 const API_KEY = process.env.NEWS_API_KEY;
 
